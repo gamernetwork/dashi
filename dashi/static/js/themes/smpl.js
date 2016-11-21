1d7dc721e26eda0330bd228593feabc63b57cd31
Mark Kennedy <mark@gamer-network.net>
2016-11-21 10:07:27 +0000

    some themes

---
+++ dashi/static/js/themes/smpl.js
@@ -0,0 +1,40 @@
+{
+  "colors": ["#d35400", "#2980b9", "#2ecc71", "#f1c40f", "#2c3e50", "#7f8c8d"],
+  "chart": {
+    "style": {
+      "fontFamily": "Roboto"
+    }
+  },
+  "title": {
+    "align": "left",
+    "style": {
+      "fontFamily": "Roboto Condensed",
+      "fontWeight": "bold"
+    }
+  },
+  "subtitle": {
+    "align": "left",
+    "style": {
+      "fontFamily": "Roboto Condensed"
+    }
+  },
+  "legend": {
+    "align": "right",
+    "verticalAlign": "bottom"
+  },
+  "xAxis": {
+    "gridLineWidth": 1,
+    "gridLineColor": "#F3F3F3",
+    "lineColor": "#F3F3F3",
+    "minorGridLineColor": "#F3F3F3",
+    "tickColor": "#F3F3F3",
+    "tickWidth": 1
+  },
+  "yAxis": {
+    "gridLineColor": "#F3F3F3",
+    "lineColor": "#F3F3F3",
+    "minorGridLineColor": "#F3F3F3",
+    "tickColor": "#F3F3F3",
+    "tickWidth": 1
+  }
+}
