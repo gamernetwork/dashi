c983079ecc983181199676524dd2f8e3762fcf70
Mark Kennedy <mark@gamer-network.net>
2018-07-05 22:15:41 +0100

    informing wsgi.py about settings layout

--- dashi/wsgi.py
+++ dashi/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mdashi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashi.settings.instance")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
