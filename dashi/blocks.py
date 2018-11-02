584985a534428fcde2d5ea1b52b72418c8420896
John McDowell <john.mcdowell@gamer-network.net>
2018-11-02 11:25:13 +0000

    CSS injecting into templates from python config file.

--- dashi/blocks.py
+++ dashi/blocks.py
@@ -172,11 +172,11 @@ class Scratch( Base ):
         self.context[ "content" ] = self.update( request )
         return render_to_string( "blocks/scratch.html", self.context, RequestContext( request ) )
 
-class Logo( Base ):
+class Title( Base ):
     #def __init__( self, block_id, conf ):
     #    super( self, Dummy ).__init__( self, block_id, conf )
     def render( self, request ):
-        return render_to_string( "blocks/logo.html", self.context, RequestContext( request ) )
+        return render_to_string( "blocks/title.html", self.context, RequestContext( request ) )
 
 class ClientInfo( Base ):
     def update( self, request ):
